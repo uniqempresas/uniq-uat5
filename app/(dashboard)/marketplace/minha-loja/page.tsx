98fe406ce179fbeb885622f3336a41c869e6c280
uniqempresas <uniq.empresas@gmail.com>
2026-03-23 11:32:15 -0300

    Fix minha-loja page - direct import

--- app/(dashboard)/marketplace/minha-loja/page.tsx
+++ app/(dashboard)/marketplace/minha-loja/page.tsx
@@ -1,10 +1,5 @@
 // app/(dashboard)/marketplace/minha-loja/page.tsx - My store dashboard
-import dynamic from 'next/dynamic';
-
-const MinhaLojaClient = dynamic(
-  () => import('@/app/(dashboard)/marketplace/minha-loja/minha-loja-client').then((mod) => mod.MinhaLojaClient),
-  { ssr: false }
-);
+import MinhaLojaClient from './minha-loja-client';
 
 export default function MinhaLojaPage() {
   return <MinhaLojaClient />;
